0e3a299b6a01d0bf4b231003336058b902084e4f
Wydode <bgcoln42@tntech.edu>
2024-09-18 13:42:33 -0500

    DriverDev
    Create first iteration of driver and switch system

---
+++ Driver.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int switchChoice;
+
+    cout << "************************************************" << endl;
+    cout << "AMUSEMENT PARK MENU" << endl;
+    cout << "What would you like to do? (Enter 1-5)" << endl;
+    cout << "Option 1: Print Rides" << endl;
+    cout << "Option 2: Delete Rides" << endl;
+    cout << "Option 3: Add Rides" << endl;
+    cout << "Option 4: End Program" << endl;
+
+    do{
+        switch(switchChoice)
+        {
+            case 1:
+                
+                break;
+            case 2:
+            
+                break;
+            case 3:
+            
+                break;
+        
+        }
+    }while(switchChoice != 4);
+	
+	cout << "\n\n*****GOODBYE! :) *****\n\n";
+    return 0;
+}
